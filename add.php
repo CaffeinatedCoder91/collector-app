5034ecc0bc983edc8840791fa873ccff9549b42d
Joanna Joseph <caffeinatedcoder91@gmail.com>
2021-02-10 13:55:01 +0000

    Trying to add images from the database to my php file

---
+++ add.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<div ="title">
+    <h1>Alcohol Free Beer Collection</h1>
+</div>
+
+
+
+<form action="html.php" method="post">
+    <div>
+        <label for="beername">Name of Beer</label>
+        <input type="text" name="" id="beername" placeholder="Your favourite beer" />
+    </div>
+
+    <div>
+        <label for="beertype">Type of Beer</label>
+        <input type="text" name="" id="beertype" placeholder="What type of beer is it" />
+    </div>
+
+    <div>
+        <label for="strength">Strength of the Beer</label>"
+        <input type="text" name="" id="strength" placeholder="It wont be very strong"/>
+    </div>
+
+    <div>
+        <label for="region">Where is the beer from?</label>
+        <input type="text" name="" id="region" placeholder="Where is it from?" />
+    </div>
+
+    <input type="submit" value="SEND"/>
+
+</form>
+
+    </html>
+
--- db.php
+++ db.php
@@ -1,31 +1,63 @@
 <?php
-//note down this line/ change only dbname
+
 $db = new PDO('mysql:host=db;dbname=collectbeer', 'root','password');
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
-
 
 $query = $db->prepare('SELECT * FROM beer');
 $query->execute();
 
 $allbeers = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
 
 
+<html>
+    <head>
+         <link rel="stylesheet" href="style.css" type="text/css"/>
+         <link rel="stylesheet" href="normalize.css" type="text/css" />
+        <title>Beer Baby</title>
+   </head>
 
+<body>
 
+    <nav>
+        <p class="title">No Fun Beer Collection</p>
+        <p class="subtitle">Joking, You Make Your Own Fun</p>
+        <p><a href="add.php">Click here</a> to add a new beer</p>
+    </nav>
 
-//fetching all the entries, storing them in a variable.
-foreach($allbeers as $beerarray)
-{
-    echo "<p> " . $beerarray["beer_name"] . "<p>";
-    echo "<p> " . $beerarray["beer_type"] . "</p>";
-    echo "<p> " . $beerarray["strength"] . "</p>";
-    echo "<p> " . $beerarray["region"]  . "</p>";
+</body>
+</html>
 
+<?php
+
+foreach($allbeers as $beerarray) {
+
+    echo '<img src="images">' . $beerarray["images"];
+
+    echo $beerarray["images"];
+
+    echo "<p> " . 'Name of beer' . ': ' . $beerarray["beer_name"] . "<p>";
+    echo "<p> " . 'What type of beer is it?' . ': ' . $beerarray["beer_type"] . "</p>";
+    echo "<p> " . 'Where is the beer from?' .  ': ' . $beerarray["region"]  . "</p>";
 
 }
+
 ?>
+
+
+
+
+
+
+
+<
+
+
+
+
+
+
 
 
 
