2a4fe2b0fb2feffa0283159c83d9d8094a772e84
Joanna Joseph <caffeinatedcoder91@gmail.com>
2021-02-11 14:56:33 +0000

    starting css

--- db.php
+++ db.php
@@ -2,45 +2,39 @@
 
 $db = new PDO('mysql:host=db;dbname=collectbeer', 'root','password');
 
+
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $query = $db->prepare('SELECT * FROM beer');
 $query->execute();
 
 $allbeers = $query->fetchAll(PDO::FETCH_ASSOC);
+require'function.php';
 ?>
 
 
 <html>
     <head>
-         <link rel="stylesheet" href="style.css" type="text/css"/>
-         <link rel="stylesheet" href="normalize.css" type="text/css" />
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="stylesheet" href="normalize.css" type="text/css">
         <title>Beer Baby</title>
    </head>
 
 <body>
 
-    <nav>
+    <nav class="navbar">
         <p class="title">No Fun Beer Collection</p>
-        <p class="subtitle">Joking, You Make Your Own Fun</p>
-        <p><a href="add.php">Click here</a> to add a new beer</p>
+        <p class=""><a href="add.php">Click here</a> to add a new beer</p>
     </nav>
+
+<div class="allbeercollection">
+    <?php echo displayallBeers($allbeers); ?>
+</div>
+
 
 </body>
 </html>
 
-<?php
-
-foreach($allbeers as $beerarray) {
-
-    echo '<img src="' .$beerarray["images"].' " >';
-    echo "<p> " . 'Name of beer' . ': ' . $beerarray["beer_name"] . "<p>";
-    echo "<p> " . 'What type of beer is it?' . ': ' . $beerarray["beer_type"] . "</p>";
-    echo "<p> " . 'Where is the beer from?' .  ': ' . $beerarray["region"]  . "</p>";
-
-}
-
-?>
 
 
 
@@ -48,7 +42,10 @@ foreach($allbeers as $beerarray) {
 
 
 
-<
+
+
+
+
 
 
 
--- function.php
+++ function.php
@@ -1,1 +1,19 @@
 <?php
+""
+function displayallBeers($allbeers){
+    $itemCard = '';
+    foreach($allbeers as $beerarray) {
+
+        $itemCard .=  '<div><img src="' . $beerarray["images"]. '" >';
+        $itemCard .= '<p> Name:' .  $beerarray["beer_name"] . '<p>';
+        $itemCard .=  '<p> Type of beer:' . $beerarray["beer_type"] . '</p>';
+        $itemCard .=  '<p> Origin of beer:' . $beerarray["region"] . '</p></div>';
+    }
+     return $itemCard;
+}
+
+?>
+
+
+
+
